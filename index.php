6119ff06513ddd2c0961561596218e18953a1f38
Ernest Neijenhuis PA3HCM <ernest@utreg.net>
2020-07-23 11:37:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
-<h1>PHP test</h1>
-
 <?php
+
+if (isset($_ENV["HOSTNAME"])) {
+    $MachineName = $_ENV["HOSTNAME"];
+} else {
+  $MachineName = 'unknown';
+}
+
+print '<h1>phpinfo() for instance ';
+print $MachineName;
+print '</h1>';
+
 phpinfo();
 ?>
